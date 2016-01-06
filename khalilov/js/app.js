e27971e757997149f99e5a1d0126d44471bfcf12
Andrii <al@gogmat.com>
2016-01-06 12:15:36 +0200

    fix

--- khalilov/js/app.js
+++ khalilov/js/app.js
@@ -13,31 +13,31 @@ jQuery(document).ready(function($){
 });
 
 $(function() {
-  $('#what, #what-mob').click(function(){
+  $('.what').click(function(){
     var destination = $("#understanding").offset().top - 50;
     $("body,html").animate({ scrollTop: destination}, 500 );
   });
-  $('#who, #who-mob').click(function(){
+  $('.who').click(function(){
     var destination = $("#training").offset().top - 50;
     $("body,html").animate({ scrollTop: destination}, 500 );
   });
-  $('#task, #task-mob').click(function(){
+  $('.task').click(function(){
     var destination = $("#business_task").offset().top - 10;
     $("body,html").animate({ scrollTop: destination}, 500 );
   });
-  $('#progr, #progr-mob').click(function(){
+  $('.progr').click(function(){
     var destination = $("#program").offset().top - 50;
     $("body,html").animate({ scrollTop: destination}, 500 );
   });
-  $('#coach, #coach-mob').click(function(){
+  $('.coach').click(function(){
     var destination = $("#training_leads").offset().top - 50;
     $("body,html").animate({ scrollTop: destination}, 500 );
   });
-  $('#bonus, #bonus-mob').click(function(){
+  $('.bonus').click(function(){
     var destination = $("#question").offset().top - 5;
     $("body,html").animate({ scrollTop: destination}, 500 );
   });
-  $('#reg, #reg-mob,.register-btn-head, .register-btn, .registr-btn').click(function(){
+  $('.reg,.register-btn-head, .register-btn, .registr-btn').click(function(){
     var destination = $("#registration").offset().top - 5;
     $("body,html").animate({ scrollTop: destination}, 500 );
   });
