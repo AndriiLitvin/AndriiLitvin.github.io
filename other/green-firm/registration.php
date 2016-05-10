ce12586f8cf569d5be226374e35b1e89eb21fe1a
Andrii Litvin <al@gogmat.com>
2016-05-10 11:39:38 +0300

    fix

--- other/green-firm/registration.php
+++ other/green-firm/registration.php
@@ -1,31 +1,17 @@
+
 <?php 
-function getVar($name)
-{
-	return isset($_POST[$name]) ? trim($_POST[$name]) : null;
-}
 
-$data=array(
-	'name' 	 => getVar('name'),
-	'email'  => getVar('email'),
-	'phone'  => getVar('phone')
-);
+$message = "Регистрация со следующими данными: \n";
+$message .= "Имя: {$_POST['name']} \n";
+$message .= "Email: {$_POST['email']} \n";
+// $message .= "Телефон: {$_POST['phone']} \n";
+$message .= "Skype: {$_POST['skype']} \n";
 
-$to = implode(",", [
-	"info@greenfirm.pro",
-]);
-
+$to = "baskakov.green@yandex.com";
 $headers = "Content-type: text/plain;charset=utf-8"; 
 $subject = "=?UTF-8?B?".base64_encode("Заявка на регистрацию")."?=";
 
-$send_data = $data;
-unset($send_data['name']);
 
-array_walk($send_data, function(&$val, $key) {
-	$val = "$key: $val";
-});
-
-$message = implode("\r\n", $send_data);
-
-$status = mail($to, $subject, $message);
-
+$status = mail($to, $subject, $message); 
 header('Location: /nextstep/index.html');
+?>
